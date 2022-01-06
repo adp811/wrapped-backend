318a8b4d00f4a4391edc3661cbfa8e4d2c254248
Shiva Ganapathy <sganapa3@ncsu.edu>
2022-01-06 16:56:17 -0500

    Fixed /all route

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const cors = require("cors");
-require("dotenv").config();
+const cors = require('cors');
+require('dotenv').config();
 
-const userDataRoutes = require("./routes/userDataRoutes");
-const categorieRoutes = require("./routes/categorieRoutes");
+const userDataRoutes = require('./routes/userDataRoutes');
+const categorieRoutes = require('./routes/categorieRoutes');
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/user", userDataRoutes);
-app.use("/api/categories", categorieRoutes);
+app.use('/api/user', userDataRoutes);
+app.use('/api/categories', categorieRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Successfully served on port: ${PORT}.`);
--- routes/userDataRoutes.js
+++ routes/userDataRoutes.js
@@ -1,35 +1,30 @@
 const router = require('express').Router();
 const data = require('../data/data');
+const fs = require('fs');
 
-router.get('/:userID/accounts', (req, res) =>{
+router.get('/:userID/accounts', (req, res) => {
+  const userID = req.params.userID;
 
-    const userID = req.params.userID;
-
-    res.send(data.users[userID].accounts);
-
+  res.send(data.users[userID].accounts);
 }); // returns all accounts for a specific user
 
 router.get('/:userID/:accountID/transactions', (req, res) => {
+  const userID = req.params.userID;
+  const accountID = req.params.accountID;
 
-    const userID = req.params.userID;
-    const accountID = req.params.accountID;
-
-    res.send(data.users[userID].accounts[accountID].transactions);
-
-}) // returns all transactions for a specific account
+  res.send(data.users[userID].accounts[accountID].transactions);
+}); // returns all transactions for a specific account
 
 router.get('/:userID/all', (req, res) => {
+  const userID = req.params.userID;
 
-    const userID = req.params.userID;
+  res.send(data.users[userID]);
+}); // returns info about a specific user
 
-    res.send(data.users[userID]);
-
-}); // returns info about a specific user 
-
-router.get('/all', (req, res) =>{
-    res.send(data);
-
+router.get('/all', (req, res) => {
+  let rawdata = fs.readFileSync('./data/data.json');
+  let returnData = JSON.parse(rawdata);
+  res.send(JSON.stringify(returnData));
 }); // returns all users
 
 module.exports = router;
-
