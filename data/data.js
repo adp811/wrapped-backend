4208acd1141734de2698c61fee7252275d5310f7
Shiva Ganapathy <sganapa3@ncsu.edu>
2022-01-06 12:29:27 -0500

    Added data

---
+++ data/data.js
@@ -0,0 +1,133 @@
+export const data = [
+  {
+    users: [
+      {
+        userID: 0,
+        userFirstName: 'John',
+        userLastName: 'Doe',
+        userEmail: 'johndoe@email.com',
+        userPassword: 'password123',
+        userDOB: '1-01-2000',
+        annualIncome: 50000,
+
+        accounts: [
+          {
+            accountId: 0,
+            accountType: 'Credit Card',
+
+            transactions: [
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+            ],
+          },
+
+          {
+            accountId: 1,
+            accountType: 'Debit Card',
+
+            transactions: [
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+            ],
+          },
+        ],
+      },
+
+      {
+        userID: 1,
+        userFirstName: '',
+        userLastName: '',
+        userEmail: '',
+        userPassword: '',
+        userDOB: '',
+        annualIncome: 0,
+
+        accounts: [
+          {
+            accountId: 0,
+            accountType: 'Credit Card',
+
+            transactions: [
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+            ],
+          },
+
+          {
+            accountId: 1,
+            accountType: 'Debit Card',
+
+            transactions: [
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+              {
+                id: '',
+                amount: '',
+                date: '',
+                category: '',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
